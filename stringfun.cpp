96cb06403f81cbe59801658d2e86b4ce23d9b237
maazcoder482 <einsteintheory777@gmail.com>
2025-06-18 13:20:21 +0530

    Add files via upload

---
+++ stringfun.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <string.h>
+using namespace std;
+int main()
+{
+ char a[50];
+ cout<<"Enter your name "<<endl;
+ gets(a);
+ cout<<"your name is "<<endl;
+ puts(a);
+ return 0;
+}
